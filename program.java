ae407e6649c2c24a72711a60453739d814502933
thikthinhick <72507300+thikthinhick@users.noreply.github.com>
2020-11-09 20:03:00 +0700

    Update program.java

--- program.java
+++ program.java
@@ -18,15 +18,5 @@ public class program {
             else
                 break;
         }
-        S
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        insertion(n, arr);
     }
 }
